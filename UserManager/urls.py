06a409c97124c40a7fd33503cdb8916c5d158fb5
Filip Matejko <matejkofilip@gmail.com>
2024-08-18 12:39:28 +0200

    Added password change. Relates to #2.

--- UserManager/urls.py
+++ UserManager/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     # path('login', views.UserLogin.as_view(), name='user_login'),
     # path('register', views.UserRegister.as_view(), name='user_register'),
     path('register', views.register, name='user_register'),
+    path('change_password', views.change_password, name='user_change_password'),
     path('profile', views.profile, name='user_profile'),
 ]
--- UserManager/views.py
+++ UserManager/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, PasswordChangeForm
+from django.contrib import messages
+from django.contrib.auth import update_session_auth_hash
 
 
 def register(request):
@@ -7,10 +9,30 @@ def register(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, 'Registration successful!')
             return redirect('login')
+        else:
+            messages.error(request, 'Registration failed!')
+            return render(request, 'UserManager/register.html', context={"form": form})
     else:
         form = UserCreationForm()
         return render(request, 'UserManager/register.html', context={"form": form})
 
 def profile(request):
     return render(request, 'UserManager/profile.html')
+
+def change_password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)  # Important!
+            messages.success(request, 'Your password was successfully updated!')
+            return redirect('user_profile')
+        else:
+            messages.error(request, 'Please correct the error below.')
+    else:
+        form = PasswordChangeForm(request.user)
+    return render(request, 'UserManager/change_password.html', {
+        'form': form
+    })
